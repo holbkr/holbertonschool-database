211483eb538757892f979b91a18efb3806b82e05
E <emilakbarov857@gmail.com>
2026-03-13 15:26:54 +0400

    asd

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,12 @@
--- taskim0
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
+-- -- taskim0
+-- DROP TABLE IF EXISTS users;
+-- CREATE TABLE users (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     email VARCHAR(255) NOT NULL UNIQUE,
+--     name VARCHAR(255)
+-- );
+-- Create table users with id, email and name
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
